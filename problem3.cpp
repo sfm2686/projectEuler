7f99ddac72524b4173b6edbcc19c301fb82c7541
Sultan <sfm2686@g.rit.edu>
2016-05-21 17:01:14 -0400

    wrote function to check if numbers are prime, problem 3

--- problem3.cpp
+++ problem3.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
+#include <math.h>
+using namespace std;
 
+bool isPrime(int num);
 int main(){
 
-	int in = 0;
+	int in;
 	cout << "Enter a number to find biggest factor" << endl;
 	cin >> in;
-	cout << "testing git" << endl;
+	cout << "The number entered was: " << in << endl;
+	bool result = isPrime(in);
+	if ( result )
+		cout << "The number is prime" << endl;
+	else
+		cout << "The number is NOT prime " << endl;
 	return 0;
+}
+
+bool isPrime(int num){
+	for ( int i = 2; i < num; i ++ )
+		if ( num % i == 0 && i != num )
+			return false;
+	return true;
 }
