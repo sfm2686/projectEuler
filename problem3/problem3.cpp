0b8f014873bc41c86104ca51d05abd5ab6e21bc3
Sultan Mira <sfm2686@g.rit.edu>
2016-05-23 19:12:50 -0400

    Solved problem 3

--- problem3/problem3.cpp
+++ problem3/problem3.cpp
@@ -2,23 +2,46 @@
 #include <math.h>
 using namespace std;
 
-bool isPrime(int num);
+bool isPrime(long num);
 int main(){
 
-	int in;
-	cout << "Enter a number to find biggest factor" << endl;
+	long in;
+	cout << "Enter a number to find biggest factor: ";
 	cin >> in;
-	cout << "The number entered was: " << in << endl;
-	bool result = isPrime(in);
-	if ( result )
+	cout << "The number entered is: " << in << endl;
+	if ( isPrime(in) )
 		cout << "The number is prime" << endl;
-	else
-		cout << "The number is NOT prime " << endl;
+	else{
+		long result = in;
+		int factor = 0;
+		for ( int i = 2; i < (in / 2) + 1; i ++ ){
+			if ( isPrime(i) && result % i == 0 ){
+				result /= i;
+				while ( result % i == 0 ){
+					result /= i;
+				}
+
+				if ( isPrime(result) ){
+					if ( result > i && result > factor )
+						factor = result;
+					else
+						factor = i;
+					break;
+				}
+				if ( i > factor )
+					factor = i;
+			}
+		}
+		cout << "The biggest prime factor in " << in << " is "
+		<< factor << endl;
+	}
 	return 0;
 }
 
-bool isPrime(int num){
-	for ( int i = 2; i < num; i ++ )
+bool isPrime(long num){
+	if ( num != 2 && num % 2 == 0 )
+		return false;
+	for ( int i = 2; i < (num / 2) + 1; i ++ )
 		if ( num % i == 0 && i != num )
 			return false;
 	return true;
