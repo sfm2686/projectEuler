9cb532612d7bd553dc9ee2732a8b8d9bb2e2a062
Sultan Mira <sfm2686@g.rit.edu>
2016-05-24 00:52:50 -0400

    Added questions to each problem, solved problem 4

---
+++ problem4/problem4.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+bool isPali(int num);
+int main(){
+
+	int pali;
+	cout << "Enter a number :";
+	cin >> pali;
+
+	if ( isPali(pali) )
+		cout << "The number is pali" << endl;
+	else
+		cout << "The number is NOT pali\n" << endl; 
+
+	int biggest = 0;
+	for ( int i = 999; i >= 100; i -- )
+		for ( int j = 999; j >= 100; j -- ){
+			if ( isPali(i * j) )
+				if ( (i * j) > biggest )
+					biggest = i * j;
+		}
+
+	cout << "The biggest pali product of 3 digits is: " << biggest << endl;
+	return 0;
+}
+
+bool isPali(int num){
+
+	stringstream conv;
+	conv << num;
+	string s = conv.str();
+
+	int len = s.length();
+	string one = "";
+	for ( int i = 0; i < len; i ++ ){
+		if ( s[i] != '\n' )
+			one += s[i];
+	}
+	string two = "";
+	for ( int i = len - 1; i > -1; i --){
+		if ( s[i] != '\n' )
+			two += s[i];
+	}
+
+	for ( int i = 0; i < len; i ++ )
+		if ( one[i] != two[i] )
+			return false;
+	return true;
+}
