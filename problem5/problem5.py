b3f9b8c08c86e8b3eff160b20cdff310862ee6d0
Sultan Mira <sfm2686@g.rit.edu>
2016-05-24 03:44:27 -0400

    modified problem 5

--- problem5/problem5.py
+++ problem5/problem5.py
@@ -1,14 +1,14 @@
 def isDiv(num):
-	for i in range(1, 21):
+	for i in range(10, 21):
 		if not(num % i == 0):
 			return False
 	return True
 
 def main():
-	i = 2
+	i = 5
 	while not(isDiv(i)):
-		i = i + 2
-	print "The number that divides range [1,20] is %d" % i
+		i = i + 5
+	print "The number that divides in the range [1,20] is %d" % i
 
 main()
 
