a3045eac48f603b52f96d9ebd0e7e6f09c1a7dae
Sultan Mira <sfm2686@g.rit.edu>
2016-05-27 03:51:35 -0400

    Solved problem12

--- problem12/problem12.cpp
+++ problem12/problem12.cpp
@@ -1,23 +1,44 @@
 #include <iostream>
 using namespace std;
 
-int findTrig(int num);
+long findTrig(int num);
+int findDivs(int num);
 
 int main(){
 
-	int n;
-	cout << "Enter a value for N: ";
-	cin >> n;
-	cout << "The " << n << "th trig is: "
-	<< findTrig(n) << endl;
+	int limit;
+	long n;
+
+	cout << "Enter a limit for divs: ";
+	cin >> limit;
+
+	int i = 1;
+	n = findTrig(i);
+	while (findDivs(n) <= limit)
+		n = findTrig(i++);
+
+	cout << "The first trig to have over " << limit 
+	<< " divs is The " << (i - 1) << "th with the value of "
+	<< n << endl;
 
 	return 0;
 }
 
-int findTrig(int num){
-	int trig = 0;
-	for (int i = 0; i <= num; i++){
-		trig += i;
-	}
+long findTrig(int num){
+	int trig = (num * (num + 1) )/2;
 	return trig;
+}
+
+int findDivs(int num){
+	int divs = 2;
+	int sum = num;
+	for (int i = 2; i < sum; i++)
+		if ( num % i == 0 ){
+			sum = num / i;
+			if ( sum == i )
+				divs ++;
+			else
+				divs += 2;
+		}
+	return divs;
 }
