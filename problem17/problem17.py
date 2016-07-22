7dc1643600038cd2b01c32b72e78c4da8f642f76
Sultan Mira <Muro@Sultans-MacBook-Pro.local>
2016-07-22 12:31:38 -0400

    meeh.. somewhat works ..

--- problem17/problem17.py
+++ problem17/problem17.py
@@ -1,31 +1,39 @@
 ones = ["", " one", " two", " three",
-			" four", " five", " six", " seven",
-			" eight", " nine", " ten", " eleven",
-			" twelve", " thirteen", " fourteen",
-			" fifteen", " sixteen", " seventeen",
-			" eighteen", " nineteen"]
+		" four", " five", " six", " seven",
+		" eight", " nine", " ten", " eleven",
+		" twelve", " thirteen", " fourteen",
+		" fifteen", " sixteen", " seventeen",
+		" eighteen", " nineteen"]
 
 tens = [ "", " ten", " twenty", " thirty",
 		" forty"," fifty",
 		" sixty", " seventy",
 		" eighty"," ninety"]
 
+hund = " hundred"
+
 def getNum(num):
 	if num <= 19:
 		return ones[num]
-	else:
+	elif num > 20 and num < 100:
 		return tens[num/10]
+	elif num % 100 == 0:
+		return ones[num/100] + hund
 
 
 def mapNum(num):
 	numString = ""
-###	while num < 0:
-	return None
+
+	while num % 10 != 0:
+		temp = num % 10
+		num %= 10
+		numString += getNum(temp)
+		
+	return numString
 
 def main():
 	i = input("Enter a number to map: ")
 	num = getNum(i)
-	print "The number you entered is%s" % num
-
+	print "The number you entered is:%s" % num
 
 main()
